a768beb0951b516d9c16d08608607a659a7286eb
johnmark287 <johnmark.muhando@students.jkuat.ac.ke>
2023-11-27 20:55:56 +0300

    update

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 word = "Holberton"
-first_3_letters = word[:3]
-last_2_letters = word[-2:]
+word_first_3 = word[:3]
+word_first_2 = word[-2:]
 middle_word = word[1:-1]
 print(f"First 3 letters: {first_3_letters}")
 print(f"Last 2 letters: {last_2_letters}")
